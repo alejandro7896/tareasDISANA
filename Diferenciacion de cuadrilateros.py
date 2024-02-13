85c87f424c842872386663a3baa7f99cfb78ef81
Alejandro Amavizca <alejandroaamavizcal@gmail.com>
2024-02-12 19:59:56 -0600

    12/02/2024

---
+++ Diferenciacion de cuadrilateros.py
@@ -0,0 +1,32 @@
+# AUTOR: ALEJANDRO ANTONIO AMAVIZCA LOPEZ
+# DISENIO Y ANALISIS DE ALGORITMOS
+# PROFESOR: MIGUEL BERNAL MARIN
+
+print("DIFERENCIADOR DE CUADRILATEROS")
+lados = []
+
+for x in range(4):
+    lados.append(int(input(f"Lado {x+1}: ")))
+
+ap = lados[0]
+cuadrado = False
+rectangulo = False
+print(ap)
+for l in lados:
+    if ap == l:
+        cuadrado = True
+    else:
+        cuadrado = False
+        break
+for l in lados:
+    if lados.count(l) == 2:
+        rectangulo = True
+    
+if cuadrado:
+    print("ES UN CUADRADO")
+    
+elif rectangulo:
+    print("ES UN RECTÁNGULO")
+else:
+    print("ES OTRO CUADRILATERO")
+    
